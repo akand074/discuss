7f95d9e31e722bb40891802d2c0a2af848bceb31
Andrew Kandalaft <andrewk@surveymonkey.com>
2017-06-24 17:06:19 -0400

    Learning elixir

---
+++ config/config.exs
@@ -0,0 +1,36 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "YUE10IFB4oDa+C1Kp2QKO/vfuORlEz4PbEW2rLQtW6hLQzAecbw9x/WyUuHSVhCN",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+    client_id: "6bbf5ba2068102627e05",
+    client_secret: "bd4fc1b34e2e5c0d4046292ca3a1151a04f2f23d"
